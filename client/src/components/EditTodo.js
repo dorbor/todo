755b303589709800dfa107fabdab52e698ec23bc
Doloquee Bedell <doloqueebedell71@gmail.com>
2022-04-06 14:07:18 +0000

    meadia query added

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -1,11 +1,28 @@
+import { useState, useEffect } from "react";
 import axios from "axios";
 import { Container } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
 const EditTodo = () => {
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
-  // updateTodo = (id) => {
+  const [action, setAction] = useState();
+
+  useEffect(() => {
+    loadAction();
+  }, []);
+
+  const loadAction = async () => {
+    const response = await axios.get("/api/todoAPi/:id");
+    setAction(response.data);
+  };
+
+  const handleChange = (e) => {
+    //     console.log(e.target.value);
+    setAction({ ...action, [e.target.name]: e.target.value });
+  };
+
+  // const updateTodo = () => {
   //   axios
   //     .put(`/api/todoAPi/${id}`)
   //     .then((res) => {
@@ -22,7 +39,7 @@ const EditTodo = () => {
     <div className="p-5 text-center">
       <Container>
         <h3 className="m-5">Kindly hit the update button after editing</h3>
-        <input type="text" />
+        <input type="text" onChange={(e) => handleChange(e)} value={action} />
         <button className="btn my-1">Update</button>
       </Container>
     </div>
